cb8ca154ad2c3b5105bab30a0f0b728c7aa28d96
giorgi <giorgi.janjulia@gtu.ge>
2019-09-03 02:16:42 +0400

    Rm

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-console.log(process.env.TEST)
-
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
